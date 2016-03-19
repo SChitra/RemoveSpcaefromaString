cdcecb218a31067568daec1ea349ec69d5874b21
C S <sethuchitra@live.com>
2016-03-19 15:49:58 -0700

    Remove Space from a string

---
+++ RemoveSpace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveSpace
+{
+    class Program
+    {
+        public static void RemoveSpace(string str)
+        {
+            char[] delim = { ' ' };
+            string[] strArr = str.Split(delim);
+            foreach (var space in strArr)
+            {
+                Console.Write(space);
+            }
+
+
+            //return str;
+    }
+    static void Main(string[] args)
+        {
+            string str = "I Love Icescream";
+            RemoveSpace(str);
+          
+        }
+    }
+}
